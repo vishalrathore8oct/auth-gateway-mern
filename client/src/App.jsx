feccc76a64845d6d75176905ac956af53285f780
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-02-23 14:39:23 +0530

    set up react project with tailwind css and vite bundler

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-blue-600 text-5xl underline text-center">Hello App!</h1>
+    </div>
+  )
+}
