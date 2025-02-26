48108abbc9284b6cd882e9f01dbaea4ae8b8310c
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-02-26 09:53:42 +0530

    complete client side signIn page UI part

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
             <div className="bg-slate-300">
                 <div className="flex justify-between items-center mx-auto px-32 py-5">
                     <Link to="/"><h1 className="font-bold">Auth App</h1></Link>
-                    <ul className="flex gap-4">
+                    <ul className="flex gap-16">
                         <Link to="/"><li>Home</li></Link>
                         <Link to="/log-in"><li>Log In</li></Link>
                     </ul>
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom"
+
 export default function SignIn() {
   return (
-    <div>
-      SingIn
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form className="flex flex-col gap-4">
+        <input type="text" name="username" id="username" placeholder="username" className="bg-slate-200 p-3 rounded-lg" />
+        <input type="email" name="email" id="email" placeholder="email" className="bg-slate-200 p-3 rounded-lg" />
+        <input type="password" name="password" id="password" placeholder="password" className="bg-slate-200 p-3 rounded-lg" />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80">Sign In</button>
+      </form>
+      <div className="flex gap-3 mt-5">
+        <p>Hava an Account ?</p>
+        <Link to="/log-in"><span className="text-blue-500">Log In</span></Link>
+      </div>
     </div>
   )
 }
