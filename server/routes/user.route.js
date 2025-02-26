b20fd45540250738e8e600596655b9939f2a3bb7
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-02-26 07:31:21 +0530

    create user testing controller

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,8 @@
 import express from "express";
+import { test } from "../controllers/user.controller.js";
 
 const router = express.Router()
 
-router.get("/", (req, res) => {
-    res.send("Hello Server route")
-})
+router.get("/", test)
 
 export default router
