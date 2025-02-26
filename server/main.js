69959ad87a2ddec21a0024c45cee7cf95915db08
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-02-26 07:04:45 +0530

    set env variable and make mongodb connection

--- server/main.js
+++ server/main.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log("MongoDB connected successfully");
+    
+}).catch((err) => {
+    console.log("MongoDB connected Error", err);
+    
+})
 
 const app = express();
 
